7aadeb8edef6ac0287927becd56922ce52471412
Lucy okoth <lucy.okoth@student.moringaschool.com>
2022-11-02 06:10:58 +0300

    firt commit

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,37 @@
+require_relative './module'
 class Author
-  attr_accessor :name
-
-
+  attr_reader :name
+  extend Finder
+  
+  @@all=[]
   def initialize(name)
     @name = name
-  
+    @@all<<self
   end
-
-
+  # inherited the 'find_by_name' from module Finder
+  # def self.find_by_name(name)
+  #   Author.all.find{|magazine|magazine.name==name}
+  # end
+  
+  def self.all
+    @@all
+  end
+  def articles
+    # filter from all articles; articles the have article.author==self.name
+    Article.all.filter{|article|article.author==@name}
+  end
+  def magazines
+    # from articles get magazine instances
+    articles.map{|article|article.magazine}.uniq
+  end
+  def add_article(magazine, title)
+    Article.new(self,magazine,title)
+  end
+  
+  def topic_areas
+    magazines.map{|magazine|magazine.category}.uniq
+  end
 end
+# article cannot exist without an author;cannot exist without a magazine;
+# author can write for a magazine that does not exist
+# magazine cannot exist without articles
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,43 @@
+require_relative './module'
 class Magazine
   attr_accessor :name, :category
-
-
+  extend Finder
+  @@all = []
   def initialize(name, category)
     @name = name
     @category = category
-
+    # add the initialized object to a class variable called "all"
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+  def magazine_articles
+    Article.all.filter{|article|article.magazine.name==@name}
+  end
+  def magazine_authors
+    magazine_articles.map{|article|Author.find_by_name(article.author)}
+  end
+  
+  def contributors
+    # filter from articles the articles{|article|article.magazine.name==@name}
+    # .map{|article|article.author.name}.uniq
+    magazine_authors.uniq
+  end
+  
+  # inherited the 'find_by_name' from module Finder
+  # def self.find_by_name(name)
+  #   Magazine.all.find{|magazine|magazine.name==name}
+  # end
 
+  def article_titles
+    magazine_articles.map{|article|article.title}
+  end
+
+  def contributing_authors
+    main_authors=[]
+    magazine_authors.tally.each{|key,value|value>2 && (main_authors<<key)}
+    main_authors
+  end
 end
